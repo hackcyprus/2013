575875fba12a47ff99e373ed41aa861a8f31f53f
Gearoid O'Rourke <gearoid@orourke-design.com>
2013-05-18 18:44:43 +0100

    add dates

--- js/main.js
+++ js/main.js
@@ -41,10 +41,22 @@ $.fullBleed = function(){
 };
 
 
-// Call sldeSize and fullBleed when we're ready to rock and roll  
 $(document).ready(function(){
   $.slideSize();
   $.fullBleed();
+
+  // Smooth scrolling
+  $('a[href^="#"]').on('click',function (e) {
+      e.preventDefault();
+      var target = this.hash,
+      $target = $(target);
+      $('html, body').stop().animate({
+          'scrollTop': $target.offset().top
+      }, 400, 'swing', function () {
+          window.location.hash = target;
+      });
+  });
+
 
 });
 
