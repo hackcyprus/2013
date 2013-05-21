4448d24246d5cc6f51d844e29ee37b42c35975e6
Gearoid O'Rourke <gearoid@orourke-design.com>
2013-05-21 22:43:10 +0100

    remove unused function

--- js/main.js
+++ js/main.js
@@ -14,36 +14,9 @@ $.slideSize = function(){
 
 };
 
-// Make sure that head image is always full-bleed
-$.fullBleed = function(){
-  
-  $(".full-bleed").each(function(){
-
-    var wW = $(window).outerWidth();
-    var wH = $(window).outerHeight();
-
-    if(wW > wH){
-      $(this).css({
-        "width": "110%",
-        "height": "auto"
-        });
-    }
-
-    else {
-      $(this).css({
-        "width": "auto",
-        "height": "110%"
-        });
-    }
-
-  });
-
-};
-
 
 $(document).ready(function(){
   $.slideSize();
-  $.fullBleed();
 
   // Smooth scrolling
   $('a[href^="#"]').on('click',function (e) {
@@ -57,13 +30,11 @@ $(document).ready(function(){
       });
   });
 
-
 });
 
 // Listen for window resize events, debounce and call them again.
 $(window).smartresize(function(){
   $.slideSize();
-  $.fullBleed();
 });
 
 
