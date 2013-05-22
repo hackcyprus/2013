1a743a71db9656a10c505330bb0bcde83879ce64
Gearoid O'Rourke <gearoid@orourke-design.com>
2013-05-22 01:19:34 +0100

    fix font bug

--- js/main.js
+++ js/main.js
@@ -14,9 +14,34 @@ $.fn.slideSize = function(){
 
 };
 
+WebFontConfig = {
+  custom: { families: ['deming', 'sullivan'],
+  urls: [ 'fonts/fonts.css' ] },
+  loading: function() {
+    
+  },
+  active: function() {
+     $('body').fadeIn();
+     $.fn.slideSize();
+  },
+  inactive: function() {
+    $.fn.slideSize();
+  },
+};
+(function() {
+  var wf = document.createElement('script');
+  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+  wf.type = 'text/javascript';
+  wf.async = 'true';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(wf, s);
+})();
+
+
 
 $(document).ready(function(){
-  $.fn.slideSize();
+ 
 
   // Smooth scrolling
   $('a[href^="#"]').on('click',function (e) {
