fd27afbaa9956d710f2aa2aeedd7a603282f49dd
Alex Michael <alexandros.michael@gmail.com>
2013-06-05 00:36:02 +0100

    refactoring and adding better support for share close

--- js/main.js
+++ js/main.js
@@ -1,63 +1,11 @@
-// Make slides fit screen
-$.fn.slideSize = function(){
+(function(global, hc, $) {
 
-  $(".slide").each(function() {
+  // main javascript
+  // ---------------
 
-    var contentHeight = $(this).find('.content').outerHeight();
-    var slideHeight = $(window).outerHeight();
-    var topPosition  = (slideHeight-contentHeight)/2;
+  var mousemoved = false;
 
-    $(this).css("height", slideHeight);
-    $(this).find(".content").css("top", topPosition);
-
-  });
-
-};
-
-WebFontConfig = {
-  custom: { families: ['deming', 'sullivan'],
-  urls: [ 'fonts/fonts.css' ] },
-
-  typekit: {
-    id: 'bie6xni'
-  },
-
-  active: function() {
-     $.fn.slideSize();
-     $('body').css("visibility", "visible");
-  },
-
-};
-
-(function() {
-  var wf = document.createElement('script');
-  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-  wf.type = 'text/javascript';
-  wf.async = 'true';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(wf, s);
-})();
-
-$(document).ready(function(){
-  // Smooth scrolling
-  $('a[href^="#"]').on('click',function (e) {
-      e.preventDefault();
-      var target = this.hash,
-      $target = $(target);
-      $('html, body').stop().animate({
-          'scrollTop': $target.offset().top
-      }, 400, 'swing', function () {
-          window.location.hash = target;
-      });
-  });
-
-  $(window).scroll(function () {
-    $("#slick-share").removeClass('open');
-  });
-
-  // Mailchimp form & JSONP callback
-  $('#mc_embed_signup form').on('submit', function(e) {
+  function mailchimpSubmit(e) {
     var $form = $(this);
 
     if ($form.hasClass('disabled')) return;
@@ -104,11 +52,44 @@ $(document).ready(function(){
 
     e.preventDefault();
     return false;
+  };
+
+  // Make slides fit screen
+  function slideSize() {
+    $(".slide").each(function() {
+      var contentHeight = $(this).find('.content').outerHeight();
+      var slideHeight = $(window).outerHeight();
+      var topPosition  = (slideHeight-contentHeight)/2;
+      $(this).css("height", slideHeight);
+      $(this).find(".content").css("top", topPosition);
+    });
+  };
+
+  $(global).smartresize(function(){
+    slideSize();
   });
 
-});
+  var closeShare = hc.once(function() {
+    $('#slick-share').removeClass('open');
+  });
 
-// Listen for window resize events, debounce and call them again.
-$(window).smartresize(function(){
-  $.fn.slideSize();
-});
+  var mousemove = hc.once(function() {
+    mousemoved = true;
+  });
+
+  $(global).on('mousemove', mousemove);
+
+  $(global).smartscroll(function() {
+    if (mousemoved) closeShare();
+  });
+
+  $(global).on('fonts:active', function() {
+    slideSize();
+    $('body').css('visibility', 'visible');
+  });
+
+  $('#mc_embed_signup form').on('submit', mailchimpSubmit);
+
+  $(hc.smoothScroller);
+
+}(window, window.hc, jQuery));
