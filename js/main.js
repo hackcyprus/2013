494d401de589fd9e52e40889d60c14db6844fc05
Alex Michael <alexandros.michael@gmail.com>
2013-06-04 02:04:16 +0100

    adding a mailchimp box

--- js/main.js
+++ js/main.js
@@ -39,11 +39,7 @@ WebFontConfig = {
   s.parentNode.insertBefore(wf, s);
 })();
 
-
-
 $(document).ready(function(){
-
-
   // Smooth scrolling
   $('a[href^="#"]').on('click',function (e) {
       e.preventDefault();
@@ -56,11 +52,57 @@ $(document).ready(function(){
       });
   });
 
+  // Mailchimp form & JSONP callback
+  $('#mc_embed_signup form').on('submit', function(e) {
+    var $form = $(this)
+      , $btn = $form.find('#mc-embedded-subscribe')
+      , $email = $form.find('#mce-EMAIL')
+      , val = $btn.val()
+      , deferred = $.ajax({
+          type: $form.attr('method'),
+          url: $form.attr('action'),
+          data: $form.serialize(),
+          dataType: 'jsonp',
+          jsonp: 'c',
+          contentType: 'application/json; charset=UTF-8'
+        });
+
+    $btn.val('Sending..');
+
+    deferred.done(function(data) {
+      if (data.result == 'success') {
+        $email.animate({
+          width: 0,
+          paddingLeft:0,
+          paddingRight: 0,
+          margin: 0
+        }, 200, 'swing', function() {
+          $(this).hide();
+        });
+        $form.animate({width: '17em'}, 200, 'swing');
+        $btn.animate({width: '17em'}, 200, 'swing', function() {
+          $btn.replaceWith(function() {
+            '<div/>', {html: $(this).html()}
+          });
+        });
+        $btn.removeClass('fail').val("Thanks, you're now subscribed!");
+      } else {
+        $btn.addClass('fail').val('Error! Try again.');
+        $email.addClass('fail');
+      }
+    });
+
+    deferred.fail(function() {
+      $btn.val(val);
+    });
+
+    e.preventDefault();
+    return false;
+  });
+
 });
 
 // Listen for window resize events, debounce and call them again.
 $(window).smartresize(function(){
   $.fn.slideSize();
 });
-
-
